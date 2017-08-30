3310d0abdc2efb4f85cecc742eb0a69c7e85e234
Conn332 <connor.t.day@gmail.com>
2017-08-30 15:39:53 -0400

    First Commit

---
+++ encrypt.py
@@ -0,0 +1,39 @@
+characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','.',',',';',':','[',']','{','}','(',')','+','-','/','*','>','<','=','"',' ']
+
+
+def decToTert(dec):
+    result = ""
+
+    for n in range(3,-1,-1):
+        if dec >= 2*(3**n):
+            dec -= 2*(3**n)
+            result += "2"
+        elif dec >= 3**n:
+            dec -= 3**n
+            result += "1"
+        else:
+            result += "0"
+    return result
+
+def tertToDec(tert):
+    result = 0
+
+    for n in range(0,4):
+        result += int(tert[n])*3**(3-n)
+    return result
+
+message = input("Message: ")
+
+tert = []
+for letter in message:
+    tert.append(decToTert(characters.index(letter)))
+
+cipher = ""
+temp = ""
+for position in range(0,4):
+    for letter in tert:
+        temp += letter[position]
+        if len(temp) == 4:
+            cipher += characters[tertToDec(temp)]
+            temp = ""
+print(cipher+"|Stop|")
